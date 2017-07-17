482e3a09b2c7953d3d8049efd30c5f2bcbcfc6e2
Marc <colem213@gmail.com>
2017-07-16 22:52:18 -0400

    Remove unnecessary annotations. Parse media content to capture all information

---
+++ src/main/java/io/podcentral/rss/MediaContent.java
@@ -0,0 +1,22 @@
+package io.podcentral.rss;
+
+import javax.xml.bind.annotation.XmlAttribute;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
+
+import lombok.Data;
+
+@DynamoDBDocument
+@Data
+public class MediaContent {
+  @XmlAttribute
+  private String url;
+  @XmlAttribute
+  private String type;
+  @XmlAttribute
+  private String medium;
+  @XmlAttribute
+  private String duration;
+  @XmlAttribute
+  private String lang;
+}
